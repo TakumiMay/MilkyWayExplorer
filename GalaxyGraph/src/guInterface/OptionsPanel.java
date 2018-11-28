fe4c3f6375415d039c9f6a2cce53346b3d06c322
TakumiMay <maythermyself@gmail.com>
2018-11-28 11:16:15 -0500

    changes again

--- GalaxyGraph/src/guInterface/OptionsPanel.java
+++ GalaxyGraph/src/guInterface/OptionsPanel.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -18,22 +19,22 @@ public class OptionsPanel extends JPanel implements ActionListener {
 
 	public static final String NEW_NODE = "new node";
 
-	public static final String SHORT_PATH = "dijkstra";
+	public static final String PRIM = "prim";
 
 	public static final String DIJKSTRA = "dijkstra";
-	
+
 	public static final String FLOYD = "floyd";
-	
+
 	public static final String BACK = "back";
 
 	private JButton btnNewNode;
 
-	private JButton btnShortPath;
+	private JButton btnPrim;
 
 	private JButton btnDijs;
 
 	private JButton btnFloyd;
-	
+
 	private JButton btnBack;
 
 	private ContainerOptionsPanel container;
@@ -43,7 +44,7 @@ public class OptionsPanel extends JPanel implements ActionListener {
 		setLayout(new GridBagLayout());
 		setPreferredSize(new Dimension(200, 180));
 		GridBagConstraints constrains = new GridBagConstraints();
-		
+
 		btnNewNode = new JButton("Add node");
 		btnNewNode.addActionListener(this);
 		btnNewNode.setActionCommand(NEW_NODE);
@@ -56,56 +57,56 @@ public class OptionsPanel extends JPanel implements ActionListener {
 
 		add(btnNewNode, constrains);
 
-		JLabel emptySpace = new JLabel("Hola");
+		JLabel emptySpace = new JLabel("");
 		emptySpace.setPreferredSize(new Dimension(150, 30));
 		constrains.gridy = 1;
 		add(emptySpace, constrains);
 
-		btnShortPath = new JButton("Short Path");
-		btnShortPath.addActionListener(this);
-		btnShortPath.setActionCommand(SHORT_PATH);
-		btnShortPath.setPreferredSize(new Dimension(150, 30));
+		btnPrim = new JButton("Prim");
+		btnPrim.addActionListener(this);
+		btnPrim.setActionCommand(PRIM);
+		btnPrim.setPreferredSize(new Dimension(150, 30));
 
 		constrains.gridx = 1;
 		constrains.gridy = 2;
 		constrains.gridwidth = 3;
 		constrains.gridheight = 1;
 
-		add(btnShortPath, constrains);
+		add(btnPrim, constrains);
 
-		if (container.getContainer().getGraphRepre().equals(MainPanel.LIST_GRAPH)) {
-			btnDijs = new JButton("Dijkstra");
-			btnDijs.addActionListener(this);
-			btnDijs.setActionCommand(DIJKSTRA);
-			btnDijs.setPreferredSize(new Dimension(150, 30));
+		btnDijs = new JButton("Dijkstra");
+		btnDijs.addActionListener(this);
+		btnDijs.setActionCommand(DIJKSTRA);
+		btnDijs.setPreferredSize(new Dimension(150, 30));
 
-			constrains.gridx = 1;
-			constrains.gridy = 6;
-			constrains.gridwidth = 3;
-			constrains.gridheight = 1;
+		constrains.gridx = 1;
+		constrains.gridy = 6;
+		constrains.gridwidth = 3;
+		constrains.gridheight = 1;
 
-			add(btnDijs, constrains);
-		} else if (container.getContainer().getGraphRepre().equals(MainPanel.MATRIX_GRAPH)) {
+		add(btnDijs, constrains);
+		
+		if (container.getContainer().getGraphRepre().equals(MainPanel.MATRIX_GRAPH)) {
 			btnFloyd = new JButton("Floyd Warshall");
 			btnFloyd.addActionListener(this);
 			btnFloyd.setActionCommand(FLOYD);
 			btnFloyd.setPreferredSize(new Dimension(150, 30));
 
 			constrains.gridx = 1;
-			constrains.gridy = 6;
+			constrains.gridy = 7;
 			constrains.gridwidth = 3;
 			constrains.gridheight = 1;
 
 			add(btnFloyd, constrains);
 		}
-		
+
 		btnBack = new JButton("Volver");
 		btnBack.addActionListener(this);
 		btnBack.setActionCommand(BACK);
 		btnBack.setPreferredSize(new Dimension(150, 30));
 
 		constrains.gridx = 1;
-		constrains.gridy = 7;
+		constrains.gridy = 8;
 		constrains.gridwidth = 3;
 		constrains.gridheight = 1;
 
@@ -118,13 +119,15 @@ public class OptionsPanel extends JPanel implements ActionListener {
 		if (command.equals(DIJKSTRA)) {
 			container.getContainer().openDjWindow();
 		} else if (command.equals(FLOYD)) {
-			
+			String floyd = "";
+			JOptionPane.showMessageDialog(this, floyd, "Floyd", JOptionPane.PLAIN_MESSAGE);
 		} else if (command.equals(BACK)) {
 			container.getContainer().back();
-		} else if (command.equals(SHORT_PATH)) {
-			
+		} else if (command.equals(PRIM)) {
+			String prim = "La ruta Prim es: ";
+			JOptionPane.showMessageDialog(this, prim, "Prim", JOptionPane.PLAIN_MESSAGE);
 		} else if (command.equals(NEW_NODE)) {
-			
+
 		}
 	}
 
